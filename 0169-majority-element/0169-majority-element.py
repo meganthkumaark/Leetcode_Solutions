1a08b9af3538872558bc68f8747faf770de81cd3
meganthkumaark <139152265+meganthkumaark@users.noreply.github.com>
2024-07-29 18:41:32 +0530

    Time: 131 ms (99.85%), Space: 17.9 MB (61.02%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        majority = None
+        
+        for num in nums:
+            if count == 0:
+                majority = num
+            count += (1 if num == majority else -1)
+        
+        return majority
